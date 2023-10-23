780277cfd733594df8cc563197bb8eac71ac18ef
Agustin Soria <soriaagustin8@gmail.com>
2023-10-23 15:33:07 -0300

    Fixing linters

--- classes/Item.rb
+++ classes/Item.rb
@@ -1,25 +1,23 @@
-class Item do
-    attr_accessor :id, :author, :source, :label, :publish_date, :archived
+class Item
+  attr_accessor :id, :author, :source, :label, :publish_date, :archived
 
-    def initialize(publish_date)
-      @id = Random.rand(1..1000)
-      @genre = nil
-      @author = nil
-      @source = nil
-      @label = nil
-      @publish_date = publish_date
-      @archived = false
-    end
+  def initialize(publish_date)
+    @id = Random.rand(1..1000)
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
+    @publish_date = publish_date
+    @archived = false
+  end
 
-    def can_be_archived?
-        (Time.new.year - @publish_date.year) > 10
-    end
+  def can_be_archived?
+    (Time.new.year - @publish_date.year) > 10
+  end
 
-    def move_to_archive
-      result = can_be_archived?
-      if result
-        @archived = true
-      end
-      nil
-    end
+  def move_to_archive
+    result = can_be_archived?
+    @archived = true if result
+    nil
+  end
 end
