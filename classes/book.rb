91e01ea2731204e5195b1b9fe2b6daf987af0d2c
Agustin Soria <soriaagustin8@gmail.com>
2023-10-24 21:55:21 -0300

    Update Book class

--- classes/book.rb
+++ classes/book.rb
@@ -1,13 +1,24 @@
+require_relative 'item'
+
 class Book < Item
     attr_accessor :publisher, :cover_state, :can_be_archived
 
     def initialize(publisher, cover_state, publish_date)
-      super(publish_date)
       @publisher = publisher
       @cover_state = cover_state
+      super(publish_date)
     end
 
     def can_be_archived?
-      Item.can_be_archived? || cover_state.downcase == 'bad'
+      super || cover_state.downcase == 'bad'
+    end
+
+    def to_json(*_args)
+      {
+        id: Random.rand(1..100),
+        publisher: @publisher,
+        cover_state: @cover_state,
+        publish_date: @publish_date
+      }
     end
 end
