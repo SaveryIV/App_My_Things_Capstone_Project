0e6d004e13938a6684927fd0e1ffcb99bd309fda
Agustin Soria <soriaagustin8@gmail.com>
2023-10-24 21:56:51 -0300

    Update Label class

--- classes/label.rb
+++ classes/label.rb
@@ -4,7 +4,7 @@ class Label
     attr_reader :items
     attr_accessor :title, :color
 
-    def initialize(id ,title, color)
+    def initialize(title, color)
       @id = Random.rand(1..1000)
       @title = title
       @color = color
@@ -14,5 +14,14 @@ class Label
     def add_item(new_item)
       @items << new_item
       new_item.label = self
+    end
+
+    def to_json(*_args)
+      {
+        id: Random.rand(1..100),
+        title: @title,
+        color: @color,
+        items: @items
+      }
     end
 end
