f5e3bd93e1aa3b421cfbf2a3a5849604b73c3b1f
Agustin Soria <soriaagustin8@gmail.com>
2023-10-25 21:59:32 -0300

    fixing linters

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -3,16 +3,15 @@ require_relative '../classes/label'
 require_relative '../classes/item'
 
 describe Label do
-    it 'Should return an instance of a Label class' do
-      label = Label.new('Twitter', 'blue')
-      expect(label).to be_an_instance_of(Label)
-    end
+  it 'Should return an instance of a Label class' do
+    label = Label.new('Twitter', 'blue')
+    expect(label).to be_an_instance_of(Label)
+  end
 
-    it 'adds itself to an item\'s label property' do
-      label = Label.new('Twitter', 'blue')
-      item = Item.new(Time.now.year - 3)
-      label.add_item(item)
-      expect(item.label).to eql(label)
-    end
-
+  it 'adds itself to an item\'s label property' do
+    label = Label.new('Twitter', 'blue')
+    item = Item.new(Time.now.year - 3)
+    label.add_item(item)
+    expect(item.label).to eql(label)
+  end
 end
